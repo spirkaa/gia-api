6ccd8511d91d28c6c716287de10a45209ee2692b
spirkaa <spirkaa@gmail.com>
2017-05-27 21:14:24 +0300

    added update_db job

---
+++ apps/rcoi/jobs/hourly/update_db.py
@@ -0,0 +1,10 @@
+from django_extensions.management.jobs import HourlyJob
+
+
+class Job(HourlyJob):
+    help = "Parse xlsx from rcoi.mcko.ru and update database"
+
+    def execute(self):
+        from apps.rcoi.models import RcoiUpdater
+        RcoiUpdater().run()
+        return
