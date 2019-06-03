0ef86412c6a3289d1fda4d0fd6d5ff894f6658a7
spirkaa <spirkaa@gmail.com>
2019-06-03 20:26:13 +0300

    updated python and project requirements versions

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python /app/manage.py collectstatic --noinput
-/usr/local/bin/gunicorn config.wsgi -w 3 -b 0.0.0.0:5000 --chdir=/app
+gunicorn config.wsgi -w 4 -t 60 -b 0.0.0.0:5000 --chdir=/app
