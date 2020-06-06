6f7122b87d9f29912df07ee70b6cc1e3ba48ff94
Ilya Pavlov <piv@devmem.ru>
2020-06-06 16:30:33 +0300

    updated project requirements versions

--- config/settings/common.py
+++ config/settings/common.py
@@ -297,6 +297,7 @@ FILTERS_HELP_TEXT_FILTER = False
 CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50,
     'DEFAULT_FILTER_BACKENDS': (
