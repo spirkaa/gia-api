3a4012a9a1cf73cd879f112800359be6be277262
Ilya Pavlov <piv@devmem.ru>
2020-06-20 09:20:57 +0300

    added mobile table style

--- gia-api/apps/rcoi/tables.py
+++ gia-api/apps/rcoi/tables.py
@@ -5,10 +5,14 @@ from . import models
 
 class EmployeeTable(tables.Table):
     name = tables.TemplateColumn(
-        template_name="rcoi/cols/employee_name.html", verbose_name="ФИО"
+        template_name="rcoi/cols/employee_name.html",
+        verbose_name="ФИО",
+        attrs={"td": {"data-title": "ФИО"}},
     )
     org = tables.TemplateColumn(
-        template_name="rcoi/cols/employee_org.html", verbose_name="Место работы"
+        template_name="rcoi/cols/employee_org.html",
+        verbose_name="Место работы",
+        attrs={"td": {"data-title": "Место работы"}},
     )
 
     class Meta:
@@ -18,11 +22,16 @@ class EmployeeTable(tables.Table):
 
 
 class PlaceTable(tables.Table):
+    code = tables.Column(attrs={"td": {"data-title": "Код ППЭ"}})
     name = tables.TemplateColumn(
-        template_name="rcoi/cols/place_name.html", verbose_name="Наименование ППЭ"
+        template_name="rcoi/cols/place_name.html",
+        verbose_name="Наименование ППЭ",
+        attrs={"td": {"data-title": "Наименование ППЭ"}},
     )
     addr = tables.TemplateColumn(
-        template_name="rcoi/cols/place_addr.html", verbose_name="Адрес ППЭ"
+        template_name="rcoi/cols/place_addr.html",
+        verbose_name="Адрес ППЭ",
+        attrs={"td": {"data-title": "Адрес ППЭ"}},
     )
 
     class Meta:
@@ -32,21 +41,39 @@ class PlaceTable(tables.Table):
 
 
 class ExamTable(tables.Table):
-    date = tables.DateColumn(accessor="date__date")
-    level = tables.Column(accessor="level__level")
-    code = tables.Column(accessor="place__code")
+    date = tables.DateColumn(
+        accessor="date__date", attrs={"td": {"data-title": "Дата"}}
+    )
+    level = tables.Column(
+        accessor="level__level", attrs={"td": {"data-title": "Уровень"}}
+    )
+    code = tables.Column(
+        accessor="place__code", attrs={"td": {"data-title": "Код ППЭ"}}
+    )
     place = tables.TemplateColumn(
-        template_name="rcoi/cols/exam_place.html", verbose_name="Наименование ППЭ"
+        template_name="rcoi/cols/exam_place.html",
+        verbose_name="Наименование ППЭ",
+        attrs={"td": {"data-title": "Наименование ППЭ"}},
     )
     place__addr = tables.TemplateColumn(
-        template_name="rcoi/cols/exam_addr.html", verbose_name="Адрес ППЭ"
+        template_name="rcoi/cols/exam_addr.html",
+        verbose_name="Адрес ППЭ",
+        attrs={"td": {"data-title": "Адрес ППЭ"}},
     )
-    position = tables.Column(accessor="position__name")
+    position = tables.Column(
+        accessor="position__name",
+        verbose_name="Должность",
+        attrs={"td": {"data-title": "Должность"}},
+    )
     employee = tables.TemplateColumn(
-        template_name="rcoi/cols/exam_employee.html", verbose_name="ФИО"
+        template_name="rcoi/cols/exam_employee.html",
+        verbose_name="ФИО",
+        attrs={"td": {"data-title": "ФИО"}},
     )
     employee__org = tables.TemplateColumn(
-        template_name="rcoi/cols/exam_org.html", verbose_name="Место работы"
+        template_name="rcoi/cols/exam_org.html",
+        verbose_name="Место работы",
+        attrs={"td": {"data-title": "Место работы"}},
     )
 
     class Meta:
