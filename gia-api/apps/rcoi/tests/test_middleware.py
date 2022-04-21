881e0ea2e5a5ab28e6617c3c8995f368f8e6d6c8
Ilya Pavlov <piv@devmem.ru>
2022-04-21 09:14:54 +0300

    fixed warning RemovedInDjango40Warning: Passing None for the middleware get_response argument is deprecated

--- gia-api/apps/rcoi/tests/test_middleware.py
+++ gia-api/apps/rcoi/tests/test_middleware.py
@@ -1,11 +1,19 @@
 from apps.rcoi.middleware import SetBrowserCacheTimeoutMiddleware
 
 
+def dummy_get_response(request):
+    return None
+
+
+def test_dummy_get_response():
+    assert dummy_get_response("test") is None
+
+
 def test_set_browser_cache_timeout_middleware(settings, max_age_response):
     """
     Test middleware - set response header
     """
     max_age = settings.RESPONSE_CACHE_SECONDS = 123
-    middleware = SetBrowserCacheTimeoutMiddleware()
+    middleware = SetBrowserCacheTimeoutMiddleware(dummy_get_response)
     r = middleware.process_response(None, max_age_response)
     assert r["cache-control"] == f"max-age={max_age}"
